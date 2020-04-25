fb5429bcbf8148e619de48a6b5f2e67612f5f216
JF Caringi <47096567+MrCaringi@users.noreply.github.com>
2020-04-25 10:04:50 -0500

    Add files via upload

---
+++ telegram-message.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+###############################
+#  TELEGRAM NOTIFICATION SCRIPT
+#
+#	Parametros
+#	1 Titulo
+#	2 Linea 1
+#	3 Linea 2
+#
+#	Last Modified
+#		2020-03-17
+#
+###############################
+
+## credenciales del bot
+#	El Archivo debe contener estas variables
+#
+#	APIKEY	del bot
+#	CHATID	del grupo o usuario
+
+. /home/jfc/scripts/telegram_notification.config
+
+curl -s \
+--data parse_mode=HTML \
+--data chat_id=$CHATID \
+--data text="<b>${1}</b>%0A      <i>from <b>`hostname`</b></i>%0A%0A${2}%0A${3}" \
+"https://api.telegram.org/bot$APIKEY/sendMessage"
+
+exit 0
